3fec98bbfafeeb281dfadf737c3a002f3b7ed938
Antonino Bonanni <maab@at1-11.bullx>
2021-05-11 09:41:51 +0000

    split infero env and python scripts env

--- env.sh
+++ env.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # ======= Basic config =========
-CONDA_DIR=${HOME}/miniconda3
-ENV_NAME=ml_exec_env2
+ROOT_SRC_DIR=${HPCPERM}/local
+ROOT_BUILD_DIR=${HPCPERM}/builds
 
-ROOT_SRC_DIR=${HOME}/local2
-ROOT_BUILD_DIR=${HOME}/builds2
+WITH_ONNX_RUNTIME=ON
+WITH_TFLITE_RUNTIME=ON
+WITH_TRT=
 
 BUILD_NPROCS=8
 # ===============================
@@ -24,45 +25,21 @@ ECKIT_SRC_DIR=${ROOT_SRC_DIR}/eckit
 ECKIT_BUILD_DIR=${ROOT_BUILD_DIR}/eckit
 
 # ONNX runtime
-WITH_ONNX_RUNTIME=ON
 ONNXRT_SOURCE_DIR=${ROOT_SRC_DIR}/onnx_rt
 ONNXRT_BUILD_DIR=${ONNXRT_SOURCE_DIR}/build
 
 # TFLITE
-WITH_TFLITE_RUNTIME=ON
 TFLITE_SOURCE_DIR=${ROOT_SRC_DIR}/tensorflow
 TFLITE_BUILD_DIR=${ROOT_BUILD_DIR}/tflite
 
 # TENSORRT
 # (NB TensorRT must to be downloaded separately)
-WITH_TRT=ON
+#WITH_TRT=ON
 TRT_SOURCE_DIR=${ROOT_SRC_DIR}/TensorRT-8.0.0.3
 TRT_BUILD_DIR=${ROOT_SRC_DIR}/TensorRT-8.0.0.3
 
-# MLEXEC
+# INFERO
 SRC_DIR=$(pwd)
-BUILD_DIR=${ROOT_BUILD_DIR}/mlexec
+BUILD_DIR=${ROOT_BUILD_DIR}/infero
 # ===============================
-
-# ============ ENV ==============
-ENV_DIR=${CONDA_DIR}/envs/${ENV_NAME}
-if [ ! -d ${ENV_DIR} ]; then
-
-  echo "Creating conda env ${ENV_NAME}"
-  conda create  -y -n ${ENV_NAME} python=3.8
-  conda activate ${ENV_NAME}
-
-  echo "Installing conda deps.."
-  conda install -y cmake
-  conda install -y matplotlib
-  pip install keras2onnx
-  pip install keras
-  pip install tensorflow==2.3.1
-
-else
-
-  # activate the environment
-  source ${CONDA_DIR}/bin/activate ${ENV_NAME}
-
-fi
 
