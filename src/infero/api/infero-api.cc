03050acc9326b6a6b8c9e27d4137712eb74e7d23
Tiago Quintino <tiago.quintino@ecmwf.int>
2021-05-14 00:53:04 +0100

    Use eckit Tensor

--- src/infero/api/infero-api.cc
+++ src/infero/api/infero-api.cc
@@ -9,19 +9,43 @@
  */
 
 #include <iostream>
+#include <vector>
+
+#include "eckit/linalg/Tensor.h"
+
+std::vector<size_t> shapify(int rank, int shape[]) {
+    std::vector<size_t> result(rank);
+    for (int i = 0; i < rank; ++i) {
+        result[i] = shape[i];
+    }
+    return result;
+}
+
+//---------------------------------------------------------------------------------------------------
+
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+using eckit::linalg::TensorDouble;
+
 void infero_infer_real64(double data1[], int rank1, int shape1[], double data2[], int rank2, int shape2[]) {
+
     std::cout << "infero_infer_real64()" << std::endl;
-    for (int r = 0; r < rank1; ++r) {
-        std::cout << "shape1[" << r << "] " << shape1[r] << std::endl;
-    }
-    for (int r = 0; r < rank2; ++r) {
-        std::cout << "shape2[" << r << "] " << shape2[r] << std::endl;
-    }
+
+    TensorDouble T1(data1, shapify(rank1, shape1));
+    TensorDouble T2(data2, shapify(rank2, shape2));
+
+    std::cout << "T1 : " << T1 << std::endl;
+    std::cout << "T2 : " << T2 << std::endl;
+
+    // for (int r = 0; r < rank1; ++r) {
+    //     std::cout << "shape1[" << r << "] " << shape1[r] << std::endl;
+    // }
+    // for (int r = 0; r < rank2; ++r) {
+    //     std::cout << "shape2[" << r << "] " << shape2[r] << std::endl;
+    // }
 }
 
 #ifdef __cplusplus
