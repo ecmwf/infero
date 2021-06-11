a3a035e54046e710a014d7cc6b5bf1703f1d18b1
antonino <antoninobonanni@yahoo.it>
2021-06-11 15:59:05 +0200

    scripts - wip

--- scripts/read_datafile.py
+++ scripts/read_datafile.py
@@ -1,27 +1,42 @@
-import sys
+
+import argparse
 import numpy as np
+from load_data import load_data
 import matplotlib.pyplot as plt
 
-try:
-    assert len(sys.argv) == 3
-except AssertionError:
-    raise AssertionError(f" {len(sys.argv)} python read_data <filename> <channel>")
+if __name__ == "__main__":
+    """
+    Lightweight script to plot prediction of shape [batch, xdim, ydim, channel]
+    """
+    
+    parser = argparse.ArgumentParser("Data Reader")
+    parser.add_argument('data_file', help="Path of the data (CSV or NPY)")
+    parser.add_argument('channel',
+                        help="channel ID",
+                        default=-1,
+                        type=int)
 
-data_file = sys.argv[1]
-ich = int(sys.argv[2])
+    args = parser.parse_args()
 
-print(f"Selected: data_file {data_file}, channel {ich}")
+    print(f"Selected: data_file {args.data_file}, channel {args.channel}")
+    
+    # read data like:
+    # - batch size
+    # - img x
+    # - img y
+    # - channel
+    vv = load_data(args.data_file)[:, :, :, args.channel]
+    assert len(vv.shape) == 3
 
-vv = np.load(data_file)
-print(f"shape {vv.shape}")
-print(f"max {np.max(vv)}")
-print(f"min {np.min(vv)}")
-print(f"mean {np.mean(vv)}")
-
-print(f"\n-----------------")
-print(f"press q to exit..")
-print(f"-----------------")
-
-plt.imshow(vv[0, :, :, ich])
-plt.show()
+    print(f"shape {vv.shape}")
+    print(f"max {np.max(vv)}")
+    print(f"min {np.min(vv)}")
+    print(f"mean {np.mean(vv)}")
+    
+    print(f"\n-----------------")
+    print(f"press q to exit..")
+    print(f"-----------------")
+    
+    plt.imshow(vv[0, :, :])
+    plt.show()
 
