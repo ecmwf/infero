bde163afdb0bc56df07416bc009eee067465a6ea
antonino <antoninobonanni@yahoo.it>
2021-06-14 15:17:06 +0200

    some more comments in the examples

--- src/infero/api/examples/runner_oro_drag.F90
+++ src/infero/api/examples/runner_oro_drag.F90
@@ -13,61 +13,67 @@ use inferof
 use iso_c_binding, only : c_double, c_int, c_float, c_char, c_null_char, c_ptr
 implicit none
 
+! Command line arguments
 character(1024) :: model_path
 character(1024) :: model_type
 character(1024) :: input_path
 character(1024) :: yaml_config
 
+! handle of infero model
 type(c_ptr) :: handle
 
+! indexes and Tensor dimensions
 integer :: i,j
 integer :: input_imax, input_jmax
 integer :: output_imax, output_jmax
+
+! file IO
 integer :: ios, fu
 integer, parameter :: read_unit = 99
 
+! input and output tensors
 real(c_float), allocatable :: it2f(:,:)
 real(c_float), allocatable :: ot2f(:,:)
 
-! input size [ 8 , 191 ]
+! orographic drag model input size [ 8 , 191 ]
 input_imax = 8
 input_jmax = 191
 
-!output size [ 8 , 126 ]
+! orographic drag model output size [ 8 , 126 ]
 output_imax = 8
 output_jmax = 126
 
-! CL arguments
+! Get CL arguments
 CALL get_command_argument(1, model_path)
 CALL get_command_argument(2, model_type)
 CALL get_command_argument(3, input_path)
 
-! float
+! Allocate tensors
 allocate( it2f(input_imax,input_jmax) )
 allocate( ot2f(output_imax,output_jmax) )
 
-! read data from CSV
+! Read data from CSV
 open (action='read', file=TRIM(input_path), iostat=ios, newunit=fu)
 if (ios /= 0) stop
 do i = 1,input_imax
   read(fu, *) (it2f(i, j), j = 1, input_jmax)
 end do
 
-! ML engine config
+! YAML config string
 yaml_config = "---"//NEW_LINE('A') &
   //"  path: "//TRIM(model_path)//NEW_LINE('A') &
   //"  type: "//TRIM(model_type)//c_null_char
 
-! get a inference model handle
+! 1) get a inference model handle
 handle = infero_create_handle_from_yaml_str(yaml_config)
 
-! open the handle
+! 2) open the handle
 call infero_open_handle(handle)
 
-! run inference
+! 3) run inference
 call infero_inference( handle, it2f, ot2f )
 
-! close and delete the handle
+! 4) close and delete the handle
 call infero_close_handle( handle )
 call infero_delete_handle( handle )
 
--- src/infero/api/examples/runner_tcyclone.F90
+++ src/infero/api/examples/runner_tcyclone.F90
@@ -13,13 +13,16 @@ use inferof
 use iso_c_binding, only : c_double, c_int, c_float, c_char, c_null_char, c_ptr
 implicit none
 
+! Command line arguments
 character(1024) :: model_path
 character(1024) :: model_type
 character(1024) :: input_path
 character(1024) :: yaml_config
 
+! handle of infero model
 type(c_ptr) :: handle
 
+! indexes and Tensor dimensions
 integer :: ss, i, j, ch
 
 integer :: input_dim_0
@@ -32,34 +35,36 @@ integer :: output_dim_1
 integer :: output_dim_2
 integer :: output_dim_3
 
+! file IO
 integer :: ios, fu
 integer, parameter :: read_unit = 99
 
+! input and output tensors
 real(c_float), allocatable :: it2f(:,:,:,:)
 real(c_float), allocatable :: ot2f(:,:,:,:)
 
-! input size [ 1, 200, 200, 17 ]
+! tcyclone model input size [ 1, 200, 200, 17 ]
 input_dim_0 = 1
 input_dim_1 = 200
 input_dim_2 = 200
 input_dim_3 = 17
 
-!output size [ 1, 200, 200, 1 ]
+! tcyclone model output size [ 1, 200, 200, 1 ]
 output_dim_0 = 1
 output_dim_1 = 200
 output_dim_2 = 200
 output_dim_3 = 1
 
-! CL arguments
+! Get CL arguments
 CALL get_command_argument(1, model_path)
 CALL get_command_argument(2, model_type)
 CALL get_command_argument(3, input_path)
 
-! float
+! Allocate tensors
 allocate( it2f(input_dim_0,input_dim_1,input_dim_2,input_dim_3) )
 allocate( ot2f(output_dim_0,output_dim_1,output_dim_2,output_dim_3) )
 
-! read 4D data from sequential CSV (in Fortran order)
+! Read 4D data from sequential CSV (CSV values are in Fortran order)
 open (action='read', file=TRIM(input_path), iostat=ios, newunit=fu)
 if (ios /= 0) stop
 do ch = 1,input_dim_3
@@ -72,26 +77,26 @@ do ch = 1,input_dim_3
     end do
 end do
 
-! ML engine config
+! YAML config string
 yaml_config = "---"//NEW_LINE('A') &
   //"  path: "//TRIM(model_path)//NEW_LINE('A') &
   //"  type: "//TRIM(model_type)//c_null_char
 
-! get a inference model handle
+! 1) get a inference model handle
 handle = infero_create_handle_from_yaml_str(yaml_config)
 
-! open the handle
+! 2) open the handle
 call infero_open_handle(handle)
 
-! run inference
+! 3) run inference
 call infero_inference( handle, it2f, ot2f )
 
-! close and delete the handle
+! 4) close and delete the handle
 call infero_close_handle( handle )
 call infero_delete_handle( handle )
 
 
-! print output
+! print output tensor (Prediction of Infero model)
 do ch = 1,output_dim_3
     do j = 1,output_dim_2
         do i = 1,output_dim_1
