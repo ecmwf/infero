9b61ce99f35e61ddc959dddaa41de92d530b93d4
antonino <antoninobonanni@yahoo.it>
2021-12-06 09:17:14 +0100

    c-api tests, wip..

--- tests/c_api/test_infero_init.cc
+++ tests/c_api/test_infero_init.cc
@@ -18,28 +18,6 @@ CASE("infero_init"){
 }
 
 
-CASE("infero_handle"){
-
-    char* arg1 = (char*)"arg1";
-    char** args;
-    args = &arg1;
-    int argc = 1;
-    int err;
-    const char* cfg_invalid = "path: /non/existent/model\ntype: invalid-engine";
-
-    infero_handle_t* h;
-    
-    EXPECT_EQUAL(infero_initialise(argc, args), INFERO_SUCCESS);
-
-    // invalid config
-    err = infero_create_handle_from_yaml_str(cfg_invalid, &h);
-    EXPECT_EQUAL(err , INFERO_ERROR_GENERAL_EXCEPTION);
-
-    EXPECT_EQUAL(infero_finalise(), INFERO_SUCCESS);
-     
-}
-
-
 int main(int argc, char* argv[]) {
     return run_tests(argc, argv);
 }
